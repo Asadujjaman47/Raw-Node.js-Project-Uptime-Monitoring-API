1b13dd0c621d6c7d15309455c7b4dd7f0b96833b
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2023-01-06 23:57:00 +0600

    Raw Node Project (Part 7)

--- index.js
+++ index.js
@@ -1,56 +1,26 @@
 /*
- * Title: Uptime Monitoring Application
- * Description: A RESTFul API to monitor up or down time of user defined links
+ * Title: Project Initial file
+ * Description: Initial file to start the node server and workers
  * Author: ASAD
  * Date: 04/01/2023
  *
  */
 
 // dependencies
-const http = require('http');
-const { handleReqRes } = require('./helpers/handleReqRes');
-const enviroment = require('./helpers/enviroments');
-const data = require('./lib/data');
-const { sendTwilioSms } = require('./helpers/notifications');
+const server = require('./lib/server');
+const workers = require('./lib/worker');
 
 // app object - module scaffolding
 const app = {};
 
-// @TODO remove later
-sendTwilioSms('01911111111', 'Hello world', (err) => {
-    console.log('this is the error', err);
-});
-
-// testing file system
-// @TODO
-// WRITE
-// data.create('test', 'newFile', { name: 'Bangladesh', language: 'Bangla' }, (err) => {
-//     console.log('error was', err);
-// });
-// READ
-// data.read('test', 'newFile', (err, data) => {
-//     console.log(err, data);
-// });
-// UPDATE
-// data.update('test', 'newFile', { name: 'England', language: 'English' }, (err) => {
-//     console.log(err);
-// });
-// DELETE
-// data.delete('test', 'newFile', (err) => {
-//     console.log(err);
-// });
-
-// create server
-
-app.createServer = () => {
-    const server = http.createServer(app.handleReqRes);
-    server.listen(enviroment.port, () => {
-        console.log(`Lisening to port ${enviroment.port}`);
-    });
+app.init = () => {
+    // start the server
+    server.init();
+    // start the workers
+    workers.init();
 };
 
-// handle Request Response
-app.handleReqRes = handleReqRes;
+app.init();
 
-// start the server
-app.createServer();
+// export the app
+module.exports = app;
